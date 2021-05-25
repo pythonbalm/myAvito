7635cd72d3273fedb0e7346afa42abb50ea13ec3
pythonbalm <78608269+pythonbalm@users.noreply.github.com>
2021-05-25 20:19:17 +0400

    1_2_2_flatpages

--- myAvito/myAvito/settings.py
+++ myAvito/myAvito/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
 ]
 
 MIDDLEWARE = [
